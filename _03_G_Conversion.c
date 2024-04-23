f9cba244e92764128012c16a7469e0b8718d30dc
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-04-23 23:32:05 +0600

    Module -10.5

---
+++ _03_G_Conversion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100001];
+    // scanf("%s", a);
+    fgets(a, sizeof(a), stdin);
+    
+    for(int i=0; i<strlen(a); i++){
+        if(a[i]==','){
+        a[i] = ' ';
+        }
+        else if(a[i]>='A' && a[i] <= 'Z'){
+            a[i]=a[i]+32;
+        }
+        else if(a[i]>='a' && a[i] <= 'z'){
+            a[i]=a[i]-32;
+        }
+        
+        
+    }
+    printf("%s", a);
+}
