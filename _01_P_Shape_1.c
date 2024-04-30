90b2f7204af536e3f32e88a3f2ca14c0eec2bb8a
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-04-30 22:54:54 +0600

    Module -14.5

---
+++ _01_P_Shape_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int k=n;
+    for(int i=1; i<=n; i++){
+        for(int j=k; j>=i; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
